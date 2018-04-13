cc23ddd32b1e4308e2a7ba1d42a2c7d8cb61ed38
Nasir <momin96@gmail.com>
2018-04-13 16:12:55 +0530

    New properties added & outlet connection

--- CoreMLDemo/CoreMLDemo/ViewController.swift
+++ CoreMLDemo/CoreMLDemo/ViewController.swift
@@ -7,19 +7,36 @@
 //
 
 import UIKit
+import AVFoundation
+import Vision
+
+//  https://blog.cmgresearch.com/2017/06/22/vision-kit-and-coreml.html
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var previewView : UIView!
+    
+    let session = AVCaptureSession()
+    
+    let captureQueue = DispatchQueue(label: "com.nasir.coreml")
+    
+    var previewLayer : AVCaptureVideoPreviewLayer!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        guard let camera = AVCaptureDevice.default(for: .video) else {
+            fatalError("No Camera availalbe")
+            return;
+        }
+        
+        session.sessionPreset = .high
+        
+        previewLayer = AVCaptureVideoPreviewLayer(session: session)
+        previewView.layer.addSublayer(previewLayer)
+    
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
